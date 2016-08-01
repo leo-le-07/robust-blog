7147779ba102ff18af8bb546f7d4f3bdbf5dcdd7
Leo Le <coder.leo.le@gmail.com>
2016-08-01 23:10:10 +0700

    remove redundant helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
-  def deny_anonymous_user
-  end
-
-  def deny_signed_in_user
-  end
-
   private
   	def current_user
   		@current_user ||= User.find(session[:user_id]) if session[:user_id]
